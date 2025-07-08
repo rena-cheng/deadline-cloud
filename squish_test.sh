38ab77803e7aea6a602bb390098e16ab9b4580bd
Rena Cheng <cheng@amazon.com>
2025-07-08 13:24:44 -0400

    test: squish ci/cd test scripts
    Signed-off-by: Rena Cheng <cheng@amazon.com>

---
+++ squish_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Set the -e option
+set -e
+
+pip install --upgrade pip
+pip install --upgrade hatch
+hatch run test_squish
